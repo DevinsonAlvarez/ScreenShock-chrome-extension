bdf3013d5105e8f0ed4d8f8cb5d9b1c53752808e
Devinson Alvarez <109635364+DevinsonAlvarez@users.noreply.github.com>
2024-01-16 16:52:16 +0000

    core(service_worker): add cominucation between the scripts

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action) {
+      console.log(request.action);
+    }
+  });
+  
--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,37 @@
-document.getElementById('record-button').addEventListener('click', async () => {
-  const media = await navigator.mediaDevices.getDisplayMedia({
-    video: { frameRate: { ideal: 30 } },
+const $recordButton = document.getElementById('record-button');
+
+// $recordButton.addEventListener('click', async () => {
+//   const media = await navigator.mediaDevices.getDisplayMedia({
+//     video: { frameRate: { ideal: 30 } },
+//   });
+
+//   const mediaRecorder = new MediaRecorder(media, {
+//     mimeType: 'video/mp4;codecs=h.264,aac',
+//   });
+
+//   mediaRecorder.start();
+
+//   const [video] = media.getVideoTracks();
+//   video.addEventListener('ended', () => {
+//     mediaRecorder.stop();
+//   });
+
+//   document
+//     .getElementById('stop-button')
+//     .addEventListener('click', () => mediaRecorder.stop());
+
+//   mediaRecorder.addEventListener('dataavailable', (e) => {
+//     const link = document.createElement('a');
+//     link.href = URL.createObjectURL(e.data);
+//     link.download = 'ScreenShock.mp4';
+//     link.click();
+//   });
+// });
+
+$recordButton.addEventListener('click', async () => {
+  const response = await chrome.runtime.sendMessage({
+    action: 'start-record',
   });
 
-  const mediaRecorder = new MediaRecorder(media, {
-    mimeType: 'video/mp4;codecs=h.264,aac',
-  });
-
-  mediaRecorder.start();
-
-  const [video] = media.getVideoTracks();
-  video.addEventListener('ended', () => {
-    mediaRecorder.stop();
-  });
-
-  document
-    .getElementById('stop-button')
-    .addEventListener('click', () => mediaRecorder.stop());
-
-  mediaRecorder.addEventListener('dataavailable', (e) => {
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(e.data);
-    link.download = 'ScreenShock.mp4';
-    link.click();
-  });
+  console.log(response);
 });
