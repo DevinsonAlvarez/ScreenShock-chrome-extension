bc672483b3dc19c1ac0b5b37f82ab423a14fd6b4
Devinson Alvarez <109635364+DevinsonAlvarez@users.noreply.github.com>
2024-01-17 17:07:12 +0000

    feat: video recorder button

--- popup/popup.js
+++ popup/popup.js
@@ -1,37 +1,9 @@
-const $recordButton = document.getElementById('record-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const $recordButton = document.getElementById("record-button");
 
-// $recordButton.addEventListener('click', async () => {
-//   const media = await navigator.mediaDevices.getDisplayMedia({
-//     video: { frameRate: { ideal: 30 } },
-//   });
-
-//   const mediaRecorder = new MediaRecorder(media, {
-//     mimeType: 'video/mp4;codecs=h.264,aac',
-//   });
-
-//   mediaRecorder.start();
-
-//   const [video] = media.getVideoTracks();
-//   video.addEventListener('ended', () => {
-//     mediaRecorder.stop();
-//   });
-
-//   document
-//     .getElementById('stop-button')
-//     .addEventListener('click', () => mediaRecorder.stop());
-
-//   mediaRecorder.addEventListener('dataavailable', (e) => {
-//     const link = document.createElement('a');
-//     link.href = URL.createObjectURL(e.data);
-//     link.download = 'ScreenShock.mp4';
-//     link.click();
-//   });
-// });
-
-$recordButton.addEventListener('click', async () => {
-  const response = await chrome.runtime.sendMessage({
-    action: 'start-record',
+  $recordButton.addEventListener("click", async () => {
+    chrome.tabs.query({ currentWindow: true, active: true }, async (tabs) => {
+      await chrome.tabs.sendMessage(tabs[0].id, { action: "start-record" });
+    });
   });
-
-  console.log(response);
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,35 @@
-console.log("Hello, I'm a injected script");
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.runtime.onMessage.addListener(
+    async (request, sender, sendResponse) => {
+      if (request.action === "start-record") {
+        const media = await navigator.mediaDevices.getDisplayMedia({
+          video: { frameRate: { ideal: 30 } },
+        });
+
+        const mediaRecorder = new MediaRecorder(media, {
+          mimeType: 'video/webm;codecs="vp8,opus"',
+        });
+        mediaRecorder.start();
+
+        const [video] = media.getVideoTracks();
+        video.addEventListener("ended", () => {
+          mediaRecorder.stop();
+        });
+
+        mediaRecorder.addEventListener("dataavailable", (e) => {
+          const locale = navigator.language;
+          const date = new Date().toLocaleString(locale, {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          });
+
+          const link = document.createElement("a");
+          link.href = URL.createObjectURL(e.data);
+          link.download = `${date}.webm`;
+          link.click();
+        });
+      }
+    }
+  );
+});
